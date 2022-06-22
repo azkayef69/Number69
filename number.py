e6cc5cbb463eb2c461b01622d1cebc93b65da9e3
azkayef69 <103218602+azkayef69@users.noreply.github.com>
2022-06-23 01:52:11 +0600

    number.py

---
+++ number.py
@@ -0,0 +1,14 @@
+import os,sys,time,hashlib,random,re
+import requests,json
+from termcolor import colored
+input ('range>>') 
+for _ in range(int(n)):
+
+
+cc='+88'
+acc='019 018 017 016 015 014 013' .split( )
+ac=random.choice(acc) 
+pc=randiom.randint(10000000,99999999)
+
+number=cc+ac+str(pc)
+print(_,number, len(number)) 
